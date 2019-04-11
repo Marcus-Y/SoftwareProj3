aa8391904324191daf73b8093cb9efa3a1c12e7f
Marcus <yaum@student.douglascollege.ca>
2019-04-10 20:03:42 -0700

    added swing application

---
+++ HelloWorld/src/swing/Swing.java
@@ -0,0 +1,49 @@
+package swing;
+
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JTextField;
+
+public class Swing extends JFrame {
+
+	private JPanel contentPane;
+	private JTextField txtMarcus;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					Swing frame = new Swing();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public Swing() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 300);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		contentPane.setLayout(new BorderLayout(0, 0));
+		setContentPane(contentPane);
+		
+		txtMarcus = new JTextField();
+		txtMarcus.setText("Marcus");
+		contentPane.add(txtMarcus, BorderLayout.CENTER);
+		txtMarcus.setColumns(10);
+	}
+
+}
