1ef203f91eda87227f434c59d1db746dae8efbcc
Jordangoulet13 <40371862+Jordangoulet13@users.noreply.github.com>
2019-04-12 04:09:48 -0700

    Add files via upload

---
+++ SoftwareProj3-iteration0/HelloWorld/src/HelloWorld.java
@@ -0,0 +1,12 @@
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello Jordan");
+		//Changed to Jordan
+		System.out.println("Hello Marcus");
+		//Added Hello Marcus.
+	}
+
+}
