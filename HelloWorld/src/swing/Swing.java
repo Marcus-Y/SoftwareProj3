141a057513c0bbcad08060782897c86c549f0794
JasonNguyen93 <47037193+JasonNguyen93@users.noreply.github.com>
2019-04-12 18:30:33 -0700

    Update Swing txt

--- HelloWorld/src/swing/Swing.java
+++ HelloWorld/src/swing/Swing.java
@@ -46,7 +46,7 @@ public class Swing extends JFrame {
 		setContentPane(contentPane);
 		
 		txtMarcus = new JTextField();
-		txtMarcus.setText("Marcus + Jason ");
+		txtMarcus.setText("Marcus + Jason");
 		contentPane.add(txtMarcus, BorderLayout.CENTER);
 		txtMarcus.setColumns(10);
 	}
